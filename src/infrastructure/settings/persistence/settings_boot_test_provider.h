366011dd1761b64b7887a4085464ac0e0b42bd5d
david-morales-33 <davidmorales33.97@gmail.com>
2025-12-14 16:55:07 -0500

    ADD: <settings_boot_test_provider>

---
+++ src/infrastructure/settings/persistence/settings_boot_test_provider.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <core/shared/interfaces/data_provider.h>
+#include <core/settings/interfaces/device_boot.h>
+
+class SettingsBootTestProvider : public IDataProvider<DeviceBoot, DeviceBoot>
+{
+private:
+    DeviceBoot boot = DeviceBoot::INSTANT_POWER;
+
+public:
+    void persist(const DeviceBoot &data) override { boot = data; }
+    void load() override {}
+    void reset() override { boot = DeviceBoot::INSTANT_POWER; }
+    void remove(const DeviceBoot &data) override {}
+    const DeviceBoot &get() const override { return boot; }
+};
