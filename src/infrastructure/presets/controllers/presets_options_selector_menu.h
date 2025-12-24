f69fb1efceedf046abca47016619c05e5df914c0
david-morales-33 <davidmorales33.97@gmail.com>
2025-12-24 17:19:17 -0500

    ADD: <presets_options_selector_menu>

---
+++ src/infrastructure/presets/controllers/presets_options_selector_menu.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <infrastructure/display.h>
+#include <core/presets/view/presets_selector.h>
+#include <core/shared/data_transfer_objects/selector.h>
+#include <core/shared/interfaces/menu_controller_params.h>
+
+class PresetsOptionsSelectorMenu : public IMenuControllerParams<int>
+{
+private:
+    Display &display;
+    PresetsSelector view;
+    Selector options_selector;
+    InterfaceState state = InterfaceState::VISIBLE;
+    int devices_selector = 0;
+
+public:
+    explicit PresetsOptionsSelectorMenu(Display &_display) : view(_display), display(_display) {}
+    void render() override
+    {
+        display.firstPage();
+        do
+        {
+            view.show(devices_selector,options_selector.getSelector());
+        } while (display.nextPage());
+    }
+    void load(const int &dev_selector = 0) override { devices_selector = dev_selector; }
+    void previous() override {options_selector.decrement(); }
+    void next() override {options_selector.increment(); }
+    void show() override { state = InterfaceState::VISIBLE; }
+    void hide() override { state = InterfaceState::HIDDEN; }
+    const InterfaceState getState() const override { return state; }
+    const int getSelector() const override { return options_selector.getSelector(); }
+};
