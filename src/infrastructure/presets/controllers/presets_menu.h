78abd079201a2fbadfa9f78cd9fb1d2a950a7671
david-morales-33 <davidmorales33.97@gmail.com>
2025-12-24 17:05:01 -0500

    ADD: <presets_menu>

---
+++ src/infrastructure/presets/controllers/presets_menu.h
@@ -0,0 +1,32 @@
+#pragma once
+#include <infrastructure/display.h>
+#include <core/presets/view/presets_view.h>
+#include <core/shared/data_transfer_objects/selector.h>
+#include <core/shared/interfaces/menu_controller_void.h>
+
+class PresetsMenu : public IMenuControllerVoid
+{
+private:
+    Display &display;
+    PresetsView view;
+    Selector selector;
+    InterfaceState state = InterfaceState::VISIBLE;
+
+public:
+    explicit PresetsMenu(Display &disp) : display(disp), view(disp), selector(2) {}
+
+    void render() override
+    {
+        display.firstPage();
+        do
+        {
+            view.show(selector.getSelector());
+        } while (display.nextPage());
+    }
+    void previous() override { selector.decrement(); }
+    void next() override { selector.increment(); }
+    void show() override { state = InterfaceState::VISIBLE; }
+    void hide() override { state = InterfaceState::HIDDEN; }
+    const InterfaceState getState() const override { return state; }
+    const int getSelector() const override { return selector.getSelector(); }
+};
