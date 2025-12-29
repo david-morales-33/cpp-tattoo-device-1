f9e97e19754ddbbcaba981fad4d8c145132e6662
david-morales-33 <davidmorales33.97@gmail.com>
2025-12-29 15:48:46 -0500

    ADD: <performance_repository_impl>

---
+++ src/infrastructure/performance/persistence/performance_repository_impl.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <core/performance/interfaces/performance_respository.h>
+#include <core/shared/interfaces/data_provider.h>
+#include <vector>
+
+class PerformanceRepositoryImpl : public IPerformanceRepository
+{
+private:
+    IDataProvider<Performance, Performance> &performance_provider;
+    IDataProvider<PerformanceRecord, std::vector<PerformanceRecord>> &record_list_provider;
+
+public:
+    explicit PerformanceRepositoryImpl(IDataProvider<Performance, Performance> &_performance_provider,
+                                       IDataProvider<PerformanceRecord, std::vector<PerformanceRecord>> &_record_list_provider) : performance_provider(_performance_provider),
+                                                                                                                                  record_list_provider(_record_list_provider) {}
+    void save(Performance performance) { performance_provider.get(); }
+    const Performance &find() const { performance_provider.get(); }
+    const std::vector<PerformanceRecord> &searchRecords() const { return record_list_provider.get(); }
+};
